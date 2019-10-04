ddeb2bda69911179baaa3869e049254427f1510e
Maksimka <52958787+andreevmaxi@users.noreply.github.com>
2019-10-04 16:22:07 +0300

    alpha2

--- test (2).cpp
+++ test (2).cpp
@@ -2,15 +2,23 @@
 #include <string>
 #include "MaksStack.h"
 
+
 int main()
     {
+    FILE* f = fopen("errors.txt", "w+");
+    fprintf(f, "sosat\n");
+    fclose(f);
+
     DedStack_t stk1;
     std::string stkname1 = "stk1";
     stk1.Name = &stkname1;
     StackInit(&stk1);
     StackPush(&stk1, 10);
+    StackPush(&stk1, -230);
+    StackPush(&stk1, -230);
     printf("%d", PopStack(&stk1));
 
     StackDestruck(&stk1);
+
     return 0;
     }
