669806d4a40eb57ba9d8ebbf697f54d26d6b9f5a
Maksimka <52958787+andreevmaxi@users.noreply.github.com>
2019-10-04 18:34:15 +0300

    gavnanavernul

--- MaksStack.h
+++ MaksStack.h
@@ -45,128 +45,6 @@ struct DedStack_t
 };
 
 /**
-    \brief NormalizeSize
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that decreeses size of our stack
-    \return 1 if all good
-    \param[in] aStack pointer on dStack with that we work
-*/
-
-bool NormalizeSize(DedStack_t* aStack);
-
-/**
-    \brief StackInit
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that initializes our stack
-    \return 1 if all good
-    \param[in] aStack pointer on dStack with that we work
-*/
-
-bool StackInit(DedStack_t* aStack);
-
-/**
-    \brief StackPush
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that push elements in our stack
-    \return 1 if all good
-    \param[in] aStack pointer on dStack with that we work
-*/
-
-bool StackPush(DedStack_t* str, StackElem_t tmp);
-
-/**
-    \brief ASSERT_OK
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that verifies our stack
-    \return 1 if all good
-    \param[in] aStack pointer on dStack with that we work
-    \param[in] NowFile file that used this stack
-    \param[in] Line line in file where that stack used
-    \param[in] FuncName function that used stack
-*/
-
-void ASSERT_OK(DedStack_t* aStack, std::string NowFile, int Line, std::string FuncName);
-
-/**
-    \brief DUMP
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that prints status of our stack
-    \return 1 if all good
-    \param[in] aStack pointer on dStack with that we work
-    \param[in] NowFile file that used this stack
-    \param[in] Line line in file where that stack used
-    \param[in] FuncName function that used stack
-*/
-
-void DUMP(DedStack_t* aStack, std::string NowFile, int Line, std::string FuncName);
-
-/**
-    \brief IsEmptyStack
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that checks if our stack is empty
-    \return 1 if it's empty and 0 if it's not empty
-    \param[in] aStack pointer on dStack with that we work
-*/
-
-bool IsEmptyStack(DedStack_t* aStack);
-
-/**
-    \brief PopStack
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that pops an element from our stack
-    \return element of poped element
-    \param[in] aStack pointer on dStack with that we work
-*/
-
-StackElem_t PopStack(DedStack_t* aStack);
-
-/**
-    \brief StuckDestruck
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that destruckes our stack
-    \param[in] aStack pointer on dStack with that we work
-*/
-
-void StackDestruck(DedStack_t* aStack);
-
-/**
-    \brief StackResize
-    \author andreevmaxi
-	\version 1.0
-	\date october  2019 year
-	\copyright korobcom
-    \details This is function that resizes our stack
-    \return 1 if all good
-    \param[in] aStack pointer on dStack with that we work
-*/
-
-bool StackResize(DedStack_t* aStack);
-
-/**
     \brief StackHash
     \author andreevmaxi
 	\version 1.0
@@ -176,8 +54,6 @@ bool StackResize(DedStack_t* aStack);
     \return hash of our stack
     \param[in] aStack pointer on dStack with that we work
 */
-
-unsigned long long StackHash(DedStack_t* aStack);
 
 unsigned long long StackHash(DedStack_t* aStack)
     {
@@ -271,158 +147,19 @@ unsigned long long StackHash(DedStack_t* aStack)
     return HashSum;
     }
 
-bool IsEmptyStack(DedStack_t* aStack)
-    {
-
-    assert(aStack != NULL);
-
-    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-    if(aStack->NowElem == 0)
-        {
-        return 1;
-        }
-    return 0;
-    }
-
-StackElem_t PopStack(DedStack_t* aStack)
-    {
-
-    assert(aStack != NULL);
-
-    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-
-    if( aStack->SizeStack > 25 && (double)aStack->SizeStack / (double)aStack->NowElem > 1.5)
-    {
-        NormalizeSize(aStack);
-    }
-
-    if(aStack->NowElem > 0)
-        {
-            StackElem_t tmp = *( aStack->data + ( --aStack->NowElem ) );
-            *( aStack->data + aStack->NowElem ) = -230;
-            DEB(aStack->StkHash   = StackHash(aStack));
-            return (tmp);
-        }
-
-    DEB(aStack->Err = 3);
-    return (-230);
-    }
-
-bool StackInit(DedStack_t* aStack)
-    {
-
-    assert(aStack != NULL);
-
-    DEB(aStack->CanaryLeft  = 230);
-    DEB(aStack->CanaryRight = 230);
-    aStack->SizeStack = 4;
-
-    #ifdef _DEBUG
-        aStack->data = (StackElem_t*) calloc(aStack->SizeStack + 2, sizeof(StackElem_t));
-    #else
-        aStack->data = (StackElem_t*) calloc(aStack->SizeStack, sizeof(StackElem_t));
-    #endif
-    DEB(aStack->CanaryDataRight = (aStack->data + aStack->SizeStack + 1) );
-    DEB(aStack->CanaryDataLeft = aStack->data);
-    DEB(++aStack->data);
-    DEB(*aStack->CanaryDataRight = 230);
-    DEB(*aStack->CanaryDataLeft = 230);
-
-    aStack->NowElem   = 0;
-
-    DEB(aStack->StkHash   = StackHash(aStack));
-
-    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-    for(int i = 0; i < aStack->SizeStack; ++i)
-        {
-            *(aStack->data + i) = -230; // poison element
-        }
-    DEB(aStack->Err = 0);
-    DEB(aStack->StkHash   = StackHash(aStack));
-    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-
-    return 1;
-
-    }
-
-bool StackPush(DedStack_t* aStack, StackElem_t tmp)
-    {
-    assert(aStack != NULL);
-
-    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-
-    if(aStack->NowElem + 1 > aStack->SizeStack)
-        {
-        StackResize(aStack);
-        DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-        }
-    if(aStack->SizeStack < 0)
-        {
-        DEB(aStack->Err = 3);
-        DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-        }
-    *(aStack->data + aStack->NowElem) = tmp;
-    ++aStack->NowElem;
-    DEB(aStack->StkHash   = StackHash(aStack));
-
-    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
-
-    return 1;
-    }
-
-void ASSERT_OK(DedStack_t* aStack, std::string NowFile, int Line, std::string FuncName)
-    {
-    assert(aStack != NULL);
-    DEB(assert(&aStack->StkHash != NULL));
-
-    bool FlagOfSecondError = 0;
-
-    DEB(if(aStack->StkHash != StackHash(aStack)))
-        {
-        DEB(aStack->Err = 6);
-        DUMP(aStack, NowFile, Line, FuncName);
-        }
-
-    if(aStack->data == NULL)
-        {
-        DEB(aStack->Err = 5);
-        DUMP(aStack, NowFile, Line, FuncName);
-        }
-
-    DEB(if(aStack->CanaryRight != 230 || aStack->CanaryLeft != 230))
-        {
-        DEB(aStack->Err = 4);
-        DUMP(aStack, NowFile, Line, FuncName);
-        }
-
-    DEB(if(*aStack->CanaryDataRight != 230 || *aStack->CanaryDataLeft != 230))
-        {
-        DEB(aStack->Err = 4);
-        DUMP(aStack, NowFile, Line, FuncName);
-        }
-
-    for(int i = 0; i < aStack->NowElem; ++i)
-        {
-        if( *(aStack->data + i) == -230)
-            {
-            FlagOfSecondError = 1;
-            }
-        }
-
-    if(FlagOfSecondError)
-        {
-        DEB(aStack->Err = 2);
-        } else
-        {
-        DEB(aStack->Err = 0);
-        }
-
-    DEB(if(aStack->Err != 0))
-        {
-        DUMP(aStack, NowFile, Line, FuncName);
-        }
-    return;
-    }
+/**
+    \brief DUMP
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that prints status of our stack
+    \return 1 if all good
+    \param[in] aStack pointer on dStack with that we work
+    \param[in] NowFile file that used this stack
+    \param[in] Line line in file where that stack used
+    \param[in] FuncName function that used stack
+*/
 
 void DUMP(DedStack_t* aStack, std::string NowFile, int Line, std::string FuncName)
     {
@@ -487,17 +224,108 @@ void DUMP(DedStack_t* aStack, std::string NowFile, int Line, std::string FuncNam
     return;
     }
 
-void StackDestruck(DedStack_t* aStack)
-    {
-    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+/**
+    \brief ASSERT_OK
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that verifies our stack
+    \return 1 if all good
+    \param[in] aStack pointer on dStack with that we work
+    \param[in] NowFile file that used this stack
+    \param[in] Line line in file where that stack used
+    \param[in] FuncName function that used stack
+*/
 
-    while (!IsEmptyStack)
+void ASSERT_OK(DedStack_t* aStack, std::string NowFile, int Line, std::string FuncName)
+    {
+    assert(aStack != NULL);
+    DEB(assert(&aStack->StkHash != NULL));
+
+    bool FlagOfSecondError = 0;
+
+    DEB(if(aStack->StkHash != StackHash(aStack)))
         {
-        PopStack(aStack);
-        DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+        DEB(aStack->Err = 6);
+        DUMP(aStack, NowFile, Line, FuncName);
+        }
+
+    if(aStack->data == NULL)
+        {
+        DEB(aStack->Err = 5);
+        DUMP(aStack, NowFile, Line, FuncName);
+        }
+
+    DEB(if(aStack->CanaryRight != 230 || aStack->CanaryLeft != 230))
+        {
+        DEB(aStack->Err = 4);
+        DUMP(aStack, NowFile, Line, FuncName);
+        }
+
+    DEB(if(*aStack->CanaryDataRight != 230 || *aStack->CanaryDataLeft != 230))
+        {
+        DEB(aStack->Err = 4);
+        DUMP(aStack, NowFile, Line, FuncName);
+        }
+
+    for(int i = 0; i < aStack->NowElem; ++i)
+        {
+        if( *(aStack->data + i) == -230)
+            {
+            FlagOfSecondError = 1;
+            }
+        }
+
+    if(FlagOfSecondError)
+        {
+        DEB(aStack->Err = 2);
+        } else
+        {
+        DEB(aStack->Err = 0);
+        }
+
+    DEB(if(aStack->Err != 0))
+        {
+        DUMP(aStack, NowFile, Line, FuncName);
         }
     return;
     }
+
+/**
+    \brief IsEmptyStack
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that checks if our stack is empty
+    \return 1 if it's empty and 0 if it's not empty
+    \param[in] aStack pointer on dStack with that we work
+*/
+
+bool IsEmptyStack(DedStack_t* aStack)
+    {
+
+    assert(aStack != NULL);
+
+    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+    if(aStack->NowElem == 0)
+        {
+        return 1;
+        }
+    return 0;
+    }
+
+/**
+    \brief StackResize
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that resizes our stack
+    \return 1 if all good
+    \param[in] aStack pointer on dStack with that we work
+*/
 
 bool StackResize(DedStack_t* aStack)
     {
@@ -529,6 +357,17 @@ bool StackResize(DedStack_t* aStack)
         return 1;
     }
 
+/**
+    \brief NormalizeSize
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that decreeses size of our stack
+    \return 1 if all good
+    \param[in] aStack pointer on dStack with that we work
+*/
+
 bool NormalizeSize(DedStack_t* aStack)
     {
     DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
@@ -554,3 +393,145 @@ bool NormalizeSize(DedStack_t* aStack)
 
     DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
     }
+
+/**
+    \brief PopStack
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that pops an element from our stack
+    \return element of poped element
+    \param[in] aStack pointer on dStack with that we work
+*/
+
+StackElem_t PopStack(DedStack_t* aStack)
+    {
+
+    assert(aStack != NULL);
+
+    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+
+    if( aStack->SizeStack > 25 && (double)aStack->SizeStack / (double)aStack->NowElem > 1.5)
+    {
+        NormalizeSize(aStack);
+    }
+
+    if(aStack->NowElem > 0)
+        {
+            StackElem_t tmp = *( aStack->data + ( --aStack->NowElem ) );
+            *( aStack->data + aStack->NowElem ) = -230;
+            DEB(aStack->StkHash   = StackHash(aStack));
+            return (tmp);
+        }
+
+    DEB(aStack->Err = 3);
+    return (-230);
+    }
+
+/**
+    \brief StackInit
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that initializes our stack
+    \return 1 if all good
+    \param[in] aStack pointer on dStack with that we work
+*/
+
+bool StackInit(DedStack_t* aStack)
+    {
+
+    assert(aStack != NULL);
+
+    DEB(aStack->CanaryLeft  = 230);
+    DEB(aStack->CanaryRight = 230);
+    aStack->SizeStack = 4;
+
+    #ifdef _DEBUG
+        aStack->data = (StackElem_t*) calloc(aStack->SizeStack + 2, sizeof(StackElem_t));
+    #else
+        aStack->data = (StackElem_t*) calloc(aStack->SizeStack, sizeof(StackElem_t));
+    #endif
+    DEB(aStack->CanaryDataRight = (aStack->data + aStack->SizeStack + 1) );
+    DEB(aStack->CanaryDataLeft = aStack->data);
+    DEB(++aStack->data);
+    DEB(*aStack->CanaryDataRight = 230);
+    DEB(*aStack->CanaryDataLeft = 230);
+
+    aStack->NowElem   = 0;
+
+    DEB(aStack->StkHash   = StackHash(aStack));
+
+    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+    for(int i = 0; i < aStack->SizeStack; ++i)
+        {
+            *(aStack->data + i) = -230; // poison element
+        }
+    DEB(aStack->Err = 0);
+    DEB(aStack->StkHash   = StackHash(aStack));
+    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+
+    return 1;
+
+    }
+
+/**
+    \brief StackPush
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that push elements in our stack
+    \return 1 if all good
+    \param[in] aStack pointer on dStack with that we work
+*/
+
+bool StackPush(DedStack_t* aStack, StackElem_t tmp)
+    {
+    assert(aStack != NULL);
+
+    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+
+    if(aStack->NowElem + 1 > aStack->SizeStack)
+        {
+        StackResize(aStack);
+        DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+        }
+    if(aStack->SizeStack < 0)
+        {
+        DEB(aStack->Err = 3);
+        DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+        }
+    *(aStack->data + aStack->NowElem) = tmp;
+    ++aStack->NowElem;
+    DEB(aStack->StkHash   = StackHash(aStack));
+
+    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+
+    return 1;
+    }
+
+/**
+    \brief StuckDestruck
+    \author andreevmaxi
+	\version 1.0
+	\date october  2019 year
+	\copyright korobcom
+    \details This is function that destruckes our stack
+    \param[in] aStack pointer on dStack with that we work
+*/
+
+void StackDestruck(DedStack_t* aStack)
+    {
+    DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+
+    while (!IsEmptyStack)
+        {
+        PopStack(aStack);
+        DEB(ASSERT_OK(aStack, __FILE__, __LINE__, __PRETTY_FUNCTION__));
+        }
+    return;
+    }
+
